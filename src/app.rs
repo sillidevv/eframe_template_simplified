5313d82d22538874c0a3de288fedba5308861403
sillidevv <166641279+matrixxdev@users.noreply.github.com>
2025-12-31 01:24:29 +0100

    Removed persistence by default, brought back the comments I got rid of in app.rs

--- src/app.rs
+++ src/app.rs
@@ -2,15 +2,17 @@
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
+	// Example stuff:
 	label: String,
 
-	#[serde(skip)] // opt-out of serialization
+	#[serde(skip)] // This how you opt-out of serialization of a field
 	value: f32,
 }
 
 impl Default for TemplateApp {
 	fn default() -> Self {
 		Self {
+			// Example stuff:
 			label: "Hello World!".to_owned(),
 			value: 2.7,
 		}
@@ -20,23 +22,47 @@ impl Default for TemplateApp {
 impl TemplateApp {
 	/// Called once before the first frame.
 	pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-		if let Some(storage) = cc.storage {
-			eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
-		} else {
-			Self::default()
-		}
+		// This is also where you can customize the look and feel of egui using
+		// `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+		// Load previous app state (if any).
+		// if let Some(storage) = cc.storage {
+		// 	eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
+		// } else {
+		// 	Self::default()
+		// }
+
+
+		// ^^ Removed for simplication.
+		//
+		//    Uncomment the block along with the fn save block
+		//    inside the impl eframe::App for TemplateApp if you want persistence.
+		//
+		//    Make sure to also uncomment the "persistence" feature of eframe in Cargo.toml
+
+
+		Self::default() // <- Remove this if you enable persistence
 	}
 }
 
 impl eframe::App for TemplateApp {
 	/// Save state before shutdown.
-	fn save(&mut self, storage: &mut dyn eframe::Storage) {
-		eframe::set_value(storage, eframe::APP_KEY, self);
-	}
+	// fn save(&mut self, storage: &mut dyn eframe::Storage) {
+	// 	eframe::set_value(storage, eframe::APP_KEY, self);
+	// }
+
+	// ^^ Removed for simplication.
+	//
+	//    Uncomment the block along with the commented code in fn new if you want persistence
+	//    Make sure to also uncomment the "persistence" feature of eframe in Cargo.toml
 
 	/// Called each frame.
 	fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+		// Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
+		// For inspiration and more examples, go to https://emilk.github.io/egui
+
 		egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+			// The top panel is often a good place for a menu bar:
 			egui::MenuBar::new().ui(ui, |ui| {
 				ui.menu_button("File", |ui| {
 					if ui.button("Quit").clicked() {
@@ -50,6 +76,7 @@ impl eframe::App for TemplateApp {
 		});
 
 		egui::CentralPanel::default().show(ctx, |ui| {
+			// The central panel the region left after adding TopPanel's and SidePanel's
 			ui.heading("eframe template");
 
 			ui.horizontal(|ui| {
