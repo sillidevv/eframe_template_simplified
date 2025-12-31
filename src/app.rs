3752d169d6ddeb001c1be930e1f87fafe1fea013
sillidevv <166641279+matrixxdev@users.noreply.github.com>
2025-12-31 13:44:31 +0100

    Fix unused parameter error by prefixing it with _ (not sure if this is the best idea)

--- src/app.rs
+++ src/app.rs
@@ -21,7 +21,7 @@ impl Default for TemplateApp {
 
 impl TemplateApp {
 	/// Called once before the first frame.
-	pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+	pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
 		// This is also where you can customize the look and feel of egui using
 		// `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
